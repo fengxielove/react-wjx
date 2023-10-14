08fa3066a9ef6e608d3c30def07cd2fe2a33ab7e
fengxie <1015806955@qq.com>
2023-10-14 22:36:55 +0800

    初始化项目+代码格式化配置

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "aviod",
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  pritWidth: 100,
+  proseWrap: 'preserve',
+  semi: false,
+  singleQuote: true,
+  tabWidth:2,
+  useTabs: true,
+  trailingComma: 'es5',
+  parser: 'typescript',
+};
