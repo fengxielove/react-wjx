6789ef343645a844b99d05f460a0324458900e13
fengxie <1015806955@qq.com>
2023-10-15 00:02:40 +0800

    chore: 基础语法学习

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 	const [count, setCount] = useState(0)
 
+	const flag = false
+	const list = [
+		{ username: 'fengxie', name: 'fengxie' },
+		{ username: '1', name: '1' },
+	]
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
 			<div className="card">
 				<button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-				<p>
+				<p style={{ color: 'red', backgroundColor: 'white' }}>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
+			<div>
+				<ul>
+					{list.map(item => {
+						const { username, name } = item
+						return <li key={username}>{name}</li>
+					})}
+				</ul>
+			</div>
+			<div>{flag ? <p>Hello World</p> : <h1>sss</h1>}</div>
 			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	)
